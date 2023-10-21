d3b21fac801e80fbac3aa11235be774deee29ef8
Maciek Bednarski <mBednar2020@gmail.com>
2023-10-21 23:26:33 +0200

    initial version bfs

---
+++ bfs.js
@@ -0,0 +1,31 @@
+const test_graph = {
+    will: ['alice', 'bob', 'claire'],
+    bob: ['anuj', 'peggy'],
+    alice: ['peggy'],
+    claire: ['thom', 'jonny'],
+    anuj: [],
+    peggy: [],
+    thom: ['bron'],
+    jonny: [],
+    bron: [],
+};
+
+const bfs = (data) => {
+    const queue = data['will'];
+    const searchStopValue = 'bron';
+    const searched = [];
+    while (queue.length > 0) {
+        const item = queue.shift();
+        if (item === searchStopValue) {
+            console.log('Item has been found!');
+            return true;
+        } else {
+            data[item].forEach((elem) => queue.push(elem));
+            searched.push(item);
+        }
+    }
+    console.log('Not found an item');
+    return false;
+};
+
+bfs(test_graph);
