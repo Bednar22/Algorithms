da3dfd20dd08b15b315f10bbaa8caf0f25e87d0c
Maciek Bednarski <mBednar2020@gmail.com>
2023-10-22 22:36:22 +0200

    fix in bfs alg to remove already searched

--- bfs.js
+++ bfs.js
@@ -16,12 +16,14 @@ const bfs = (data) => {
     const searched = [];
     while (queue.length > 0) {
         const item = queue.shift();
-        if (item === searchStopValue) {
-            console.log('Item has been found!');
-            return true;
-        } else {
-            data[item].forEach((elem) => queue.push(elem));
-            searched.push(item);
+        if (searched.includes(item)) {
+            if (item === searchStopValue) {
+                console.log('Item has been found!');
+                return true;
+            } else {
+                data[item].forEach((elem) => queue.push(elem));
+                searched.push(item);
+            }
         }
     }
     console.log('Not found an item');
