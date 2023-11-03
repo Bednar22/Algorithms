d114e398c612b508624c7a1a21201d0f16f9b04b
Maciek Bednarski <mBednar2020@gmail.com>
2023-11-03 20:29:26 +0100

    ut for longestCommonSubseq(not implemented yet)

---
+++ tests/longestCommonSubseq.test.js
@@ -0,0 +1,25 @@
+const { longestCommonSubseq } = require('../longestCommonSubseq');
+
+const TEST_1_STR_1 = 'hish';
+const TEST_1_STR_2 = 'hysa';
+const TEST_1_RESULT = 2;
+
+const TEST_2_STR_1 = 'starq';
+const TEST_2_STR_2 = 'starm';
+const TEST_2_RESULT = 4;
+
+const TEST_3_STR_1 = 'estarq';
+const TEST_3_STR_2 = 'strm';
+const TEST_3_RESULT = 3;
+
+test('should return longest common substring', () => {
+    expect(longestCommonSubstr(TEST_1_STR_1, TEST_1_STR_2)).toEqual(TEST_1_RESULT);
+});
+
+test('should return longest common substring when in the beggining of the both string', () => {
+    expect(longestCommonSubstr(TEST_2_STR_1, TEST_2_STR_2)).toEqual(TEST_2_RESULT);
+});
+
+test('should return longest common substring when in the beggining of the 2nd string ', () => {
+    expect(longestCommonSubstr(TEST_3_STR_1, TEST_3_STR_2)).toEqual(TEST_3_RESULT);
+});
